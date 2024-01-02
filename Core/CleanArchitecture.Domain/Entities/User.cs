539c65060658835f69c01d6dc54c8974179ba10b
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-01-02 17:11:52 -0300

    First commit of CleanArchiteture lesson project

---
+++ Core/CleanArchitecture.Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+namespace Core.CleanArchitecture.Domain.Entities
+{
+    public sealed class User : BaseEntity
+    {
+        public string Email { get; set; }
+        public string? Name { get; set; }
+    }
+}
